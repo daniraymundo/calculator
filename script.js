73ae2bd83f5c913a769c585f10252d437562bc73
Dani <dmmraymundo1@gmail.com>
2023-07-31 12:22:01 +0800

    Add equals event listener

--- script.js
+++ script.js
@@ -1,51 +1,85 @@
-const previousValueDisplay = document.querySelector(".previous-value")
-const currentValueDisplay =  document.querySelector(".current-value")
+const previousValueDisplay = document.querySelector(".previous-value");
+const currentValueDisplay =  document.querySelector(".current-value");
 let equation = []; //stores previous value (value displayed in history)
 let newNumberFlag = false;
-const numbers = document.querySelectorAll(".number")
-const operators = document.querySelectorAll(".operator")
+let lastOperator = null;
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const equals = document.querySelector(".equals");
 
 numbers.forEach(number => {
     number.addEventListener("click", event => {
         const clickedNumber = event.target.textContent;
-        if (newNumberFlag) {
-            currentValueDisplay.textContent = clickedNumber
+        if (newNumberFlag && !equation.length) {
+            previousValueDisplay.textContent = "\u00A0"
+            currentValueDisplay.textContent = clickedNumber;
+        } else if (newNumberFlag) {
+            currentValueDisplay.textContent = clickedNumber;
             newNumberFlag = false;
         } else {
-            currentValueDisplay.textContent = currentValueDisplay.textContent == 0 ? clickedNumber :`${currentValueDisplay.textContent}${clickedNumber}`
+            currentValueDisplay.textContent = currentValueDisplay.textContent == 0 ? clickedNumber :`${currentValueDisplay.textContent}${clickedNumber}`;
         }
     })
 })
 
 operators.forEach(operator => {
     operator.addEventListener("click", event => {
-        const operator = event.target.textContent;
-        const currentValue = currentValueDisplay.textContent;
 
         if (newNumberFlag) {
             previousValueDisplay.textContent = "";
             equation = [];
         }
 
+        const operator = event.target.textContent;
+        const currentValue = currentValueDisplay.textContent;
+
         if (!equation.length && currentValue == 0) return;
 
         if (!equation.length) {
             equation.push(currentValue, operator);
-            previousValueDisplay.textContent = `${currentValue} ${operator}`
+            previousValueDisplay.textContent = `${currentValue} ${operator}`;
+            lastOperator = operator;
             return newNumberFlag = true;
-        }
-        if (equation.length) {
+        } else {
             equation.push(currentValue);
-            const newValue = calculate(equation[0], equation[2], equation[1])
-            currentValueDisplay.textContent = newValue;
-            previousValueDisplay.textContent = `${newValue} ${operator}`;
-            equation = [newValue, operator]
-            newNumberFlag = true;
+            const result = calculate(equation[0], equation[1], equation[2]);
+            currentValueDisplay.textContent = result;
+            previousValueDisplay.textContent = `${result} ${operator}`;
+            equation = [result, operator];
+            lastOperator = operator;
+            return newNumberFlag = true;
         }
     })
 })
 
-function calculate(operand1, operand2, operator) {
+let result;
+let newResult;
+let previousEquation = [];
+
+equals.addEventListener("click", () => {
+
+    if (!equation.length && previousEquation.length) {
+    equation = [...previousEquation]
+    newResult = calculate(result, equation[1], equation[2]);
+    currentValueDisplay.textContent = newResult;
+    previousValueDisplay.textContent = `${result} ${equation[1]} ${equation[2]} =`;
+    previousEquation = [];
+    previousEquation.push(result, equation[1], equation[2]);
+    result = newResult;
+    } else if (!equation.length)  {
+        previousValueDisplay.textContent = `${currentValueDisplay.textContent} =`;
+    } else if (equation.length) {
+        equation.push(currentValueDisplay.textContent);
+        result = calculate(equation[0], equation[1], equation[2]);
+        currentValueDisplay.textContent = result;
+        previousValueDisplay.textContent = `${equation[0]} ${equation[1]} ${equation[2]} =`;
+        previousEquation = [...equation]
+    }
+    equation = [];
+    newNumberFlag = true;
+})
+
+function calculate(operand1, operator, operand2) {
     operand1 = Number(operand1);
     operand2 = Number(operand2);
 
@@ -58,7 +92,7 @@ function calculate(operand1, operand2, operator) {
     } else if (operator === "x") {
         result = operand1 * operand2;
     } else if (operator === "÷") {
-        result = operand1 / operand2
+        result = operand2 != 0 ? operand1 / operand2 : "Cannot divide by zero";
     }
     return result;
 }
